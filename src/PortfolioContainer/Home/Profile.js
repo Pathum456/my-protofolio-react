8a873f6580d2aafa3e386867136f6600c674f9ac
pathum456 <gdse58courseonlinelessons2021@gmail.com>
2022-09-12 12:23:19 +0530

    Home page done Responsive too

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
+import Typical from 'react-typical';
+import "./Profile.css"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function () {
@@ -7,6 +10,8 @@ export default function () {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
+            <div className="colz-icon">
+           
             <a href="https://github.com/Pathum456">
               <i className=" fa fa-github-square"></i>
             </a>
@@ -19,8 +24,49 @@ export default function () {
             <a href="https://www.facebook.com/profile.php?id=100074316473313">
               <i className=" fa fa-facebook-square"></i>
             </a>
-            
+
+            </div>
           </div>
+          <div className="profile-details-name">
+            
+            {""}
+            Hello, I'M <span className="highlited-text">Pathum kaleesha</span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+                {""}
+                <h1>
+                  {" "}
+                  <Typical 
+                  loop={Infinity}
+                  steps={[
+                    "Pathum Kaleesha",
+                    2000,"Full Stack Developer..,",
+                    5000,"Mobile App Developer..,",
+                    5000,"React/React Native ",
+                    
+                  ]}></Typical>
+                </h1>
+                <span className="profile-role-tagline">
+                    knack of building application with front and back end 
+                    Operations.
+                </span>
+            </span>
+            </div>
+            <div className="profile-options">
+                <button className="btn primary-btn">
+                    {""}
+                    Hire Me{" "}
+                </button>
+               
+                <button className="btn highlighted-btn"> <a href="mybio.pdf" download={'Pathum Kaleesha.pdf'}>
+                    {""}
+                    Get Resume{" "}
+                    </a></button>
+            </div>
+        </div>
+        <div className="profile-picture">
+            <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
