2cff977ae08659f1cf1613df2cc0acf5eef74a76
pathum456 <gdse58courseonlinelessons2021@gmail.com>
2022-09-12 09:51:49 +0530

    adding icons

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function () {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://github.com/Pathum456">
+              <i className=" fa fa-github-square"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/pathum-kaleesha-166a15225/">
+              <i className=" fa fa-linkedin-square"></i>
+            </a>
+            <a href="https://twitter.com/Pathum_Kaleesha">
+              <i className=" fa fa-twitter-square"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100074316473313">
+              <i className=" fa fa-facebook-square"></i>
+            </a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
